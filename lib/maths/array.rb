b636fe18c49cfb5c6ab79437064f57136e40e213
geemus <geemus@gmail.com>
2013-02-21 09:50:46 -0600

    rename std to standard_deviation, rework using root mean square deviation

--- lib/maths/array.rb
+++ lib/maths/array.rb
@@ -21,13 +21,20 @@ module Maths
       end
     end
 
-    def self.std(array)
+    def self.root_mean_square_deviation(array, &estimator)
+      Math.sqrt(
+        average(
+          array.map(&estimator)
+        )
+      )
+    end
+
+    def self.standard_deviation(array)
       avg = average(array)
-      squared_diffs = array.map do |value|
+      root_mean_square_deviation(array) do |value|
         diff = value - avg
         diff * diff
       end
-      Math.sqrt(average(squared_diffs))
     end
 
     def self.sum(array)
@@ -48,7 +55,7 @@ if __FILE__ == $0
   p "median(#{array.inspect}): #{Maths::Array.median(array)}"
 
   array = [2, 4, 4, 4, 5, 5, 7, 9]
-  p "std(#{array.inspect}): #{Maths::Array.std(array)}"
+  p "standard_deviation(#{array.inspect}): #{Maths::Array.standard_deviation(array)}"
 
   array = 0.upto(5).to_a
   p "sum(#{array.inspect}): #{Maths::Array.sum(array)}"
